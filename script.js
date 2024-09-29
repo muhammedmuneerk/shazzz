65a9f1663b0fc82af3ff43422778635c048b2136
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2024-09-29 19:01:18 +0530

    margin-left-15%

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// @media (max-width: 768px) {
+//   .section-container {
+//     flex-direction: column;
+//     align-items: center; /* Center align items */
+//     text-align: center;  /* Center text */
+//   }
+
+//   .about__pic-container {
+//     display: center;
+//     justify-content: center;
+//     margin-bottom: 2rem; /* Add spacing below image */
+//   }
+
+//   .about-pic {
+//     width: 80%;  /* Make image responsive */
+//     max-width: 300px;  /* Ensure it doesn't get too large */
+//     border-radius: 50%; /* Make the image circular if needed */
+//   }
+// }
