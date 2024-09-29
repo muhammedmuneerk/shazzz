6c7c18961357839849dd8876784ce2c55e63e15e
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2024-09-29 19:58:15 +0530

    about__pic-container solved

--- script.js
+++ script.js
@@ -6,22 +6,3 @@ function toggleMenu() {
 }
 
 
-// @media (max-width: 768px) {
-//   .section-container {
-//     flex-direction: column;
-//     align-items: center; /* Center align items */
-//     text-align: center;  /* Center text */
-//   }
-
-//   .about__pic-container {
-//     display: center;
-//     justify-content: center;
-//     margin-bottom: 2rem; /* Add spacing below image */
-//   }
-
-//   .about-pic {
-//     width: 80%;  /* Make image responsive */
-//     max-width: 300px;  /* Ensure it doesn't get too large */
-//     border-radius: 50%; /* Make the image circular if needed */
-//   }
-// }
